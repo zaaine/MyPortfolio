4659b3216cbf98e95a14a1610eb00e1940c7e964
aziz zaaine <zaaine@live.fr>
2024-09-09 19:46:27 +0200

    Création du composant About.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,72 @@
 import React from "react";
+import "../styles/_components.scss/About.scss";
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is the About Page.</p>
+    <div className="about_container">
+      <h1 className="about_title">Bienvenue sur mon Portfolio</h1>
+      <p>
+        Je m'appelle <strong>Aziz</strong>, développeur web passionné basé à
+        Paris. Il y a 5 ans, j'ai découvert l'univers du code, et cela a
+        rapidement transformé ma carrière. Aujourd'hui, je me consacre
+        pleinement à la création de sites web fonctionnels, modernes et centrés
+        sur l'utilisateur, avec une expertise particulière en{" "}
+        <strong>React</strong> et
+        <strong>JavaScript</strong>.
+      </p>
+      <br />
+      <p>
+        Avant de me lancer dans le développement, j'ai travaillé pendant 12 ans
+        en tant que Responsable dans diverses structures, où j'ai acquis une
+        solide expérience en <strong>management</strong>. J'ai eu l'occasion de
+        collaborer avec des équipes aux profils variés, aussi bien en France
+        qu'à l'étranger, ce qui m'a permis de développer une grande adaptabilité
+        et une richesse humaine. Mon rôle consistait à placer la satisfaction
+        client au centre de mes priorités, tout en bâtissant des relations
+        solides et durables avec des personnes issues d'horizons différents.
+      </p>
+      <br />
+      <p>
+        Cette expérience dans le commerce m'a permis d'apprendre à gérer des
+        projets avec efficacité, tout en gardant l'utilisateur final au cœur de
+        mes réflexions. J'ai ensuite décidé de me professionnaliser en suivant
+        une formation de 18 mois chez <strong>OpenClassrooms</strong>, où j'ai
+        approfondi mes compétences en <strong>React</strong>,{" "}
+        <strong>JavaScript</strong>, <strong>HTML</strong>, <strong>CSS</strong>
+        ,<strong>PHP</strong>, et <strong>SASS</strong>. Grâce à cette formation
+        et à mon parcours professionnel, j'ai acquis une approche du
+        développement web axée sur des solutions simples et efficaces, en
+        m'assurant toujours que le produit final réponde parfaitement aux
+        besoins de l'utilisateur.
+      </p>
+      <br />
+      <p>
+        En dehors du travail, je suis une personne dynamique et curieuse.
+        J'adore partir en <strong>randonnée</strong>, découvrir de nouvelles
+        cultures lors de
+        <strong> voyages</strong>, et me lancer dans des projets de{" "}
+        <strong>bricolage</strong> ou de <strong>réparation</strong>. Je
+        m'engage également dans des actions de <strong>bénévolat</strong>, car
+        contribuer à des causes importantes est pour moi essentiel.
+      </p>
+      <br />
+      <p>
+        Sur ce site, je vous présente mes <strong>projets réalisés</strong> dans
+        le cadre de ma formation ainsi que des{" "}
+        <strong>projets personnels</strong>. Je continue de
+        <strong> coder régulièrement</strong> de nouveaux projets, que je mets à
+        jour dans mon dépôt <strong>GitHub</strong>, où vous pouvez suivre mon
+        évolution. Je vous invite également à explorer mon dépôt et à me suivre
+        sur les
+        <strong> réseaux sociaux</strong> pour découvrir mes dernières
+        réalisations.
+      </p>
+      <br />
+      <p>
+        N'hésitez pas à <strong>me contacter</strong> pour discuter de vos idées
+        ou de <strong>futurs projets</strong>. Je suis toujours à la recherche
+        de nouvelles collaborations créatives et enrichissantes.
+      </p>
     </div>
   );
 }
