13444ba7ac23956b3d9426c05e2ca246ee07ed00
aziz zaaine <zaaine@live.fr>
2024-09-18 23:54:44 +0200

    modification de la page description et ajout du style. Enrichissement du fichier JSON pour decrire les projets.

--- src/pages/Description.js
+++ src/pages/Description.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { Button, Tooltip, color } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import data from "../assets/BDD_JSON/presentation_projects.json";
+
+import "../styles/_pages.scss/Description.scss";
 
 // Fonction pour importer dynamiquement les images des dossiers
 const importAll = (r) =>
@@ -51,37 +54,78 @@ export default function Description() {
   const screenshotImage = screenshots[project.screenshot];
 
   return (
-    <div>
+    <div className="contenaire-description">
       {landscapeImage && (
-        <img src={landscapeImage} alt={`${project.name} Landscape`} />
+        <img
+          src={landscapeImage}
+          alt={`${project.name} Landscape`}
+          className="description-image_landscape"
+        />
       )}
-      <h1>{project.name}</h1>
-      <p>
-        <strong>Mission :</strong> {project.Mission}
-      </p>
-      <p>
-        <strong>Description :</strong> {project.description}
-      </p>
-      <p>
-        <strong>Livraison :</strong> {project.livraison}
-      </p>
-      <p>
-        <strong>Langages utilisés :</strong> {project.languages.join(", ")}
-      </p>
+      <h1 className="description_title">{project.name}</h1>
 
-      {screenshotImage && (
-        <img src={screenshotImage} alt={`${project.name} Screenshot`} />
-      )}
-      <p>
-        <a href={project.link_projet} target="_blank" rel="noopener noreferrer">
-          Voir le projet en ligne
-        </a>
-      </p>
-      <p>
-        <a href={project.Link_Git} target="_blank" rel="noopener noreferrer">
-          Voir le code sur GitHub
-        </a>
-      </p>
+      <div className="contenaire-texts">
+        <h2>
+          <strong>Mission :</strong>
+        </h2>
+
+        <p className="description_text">{project.Mission}</p>
+
+        <h2>
+          <strong>Description :</strong>
+        </h2>
+        <p className="description_text">{project.description}</p>
+
+        <h2>
+          <strong>Livraison :</strong>
+        </h2>
+
+        <p className="description_text">{project.livraison}</p>
+
+        <p className="description_text">
+          <strong>Langages utilisés :</strong> {project.languages.join(", ")}
+        </p>
+
+        {screenshotImage && (
+          <img
+            src={screenshotImage}
+            alt={`${project.name} Screenshot`}
+            className="description-image_screenshot"
+          />
+        )}
+      </div>
+
+      <div className="contenaire_buttons">
+        <Tooltip label="visiter le site" aria-label="infobulle link">
+          <Button
+            colorScheme={color}
+            bg={`${color}.400`}
+            rounded={"full"}
+            px={6}
+            _hover={{
+              bg: `${color}.500`,
+            }}
+            href={project.link_projet}
+          >
+            site Web
+          </Button>
+        </Tooltip>
+
+        <Tooltip label="voir le code Git" aria-label="infobulle link">
+          <Button
+            colorScheme={color}
+            bg={`${color}.400`}
+            rounded={"full"}
+            px={6}
+            _hover={{
+              bg: `${color}.500`,
+            }}
+            href={project.Link_Git}
+          >
+            Github
+          </Button>
+        </Tooltip>
+      </div>
     </div>
   );
 }
