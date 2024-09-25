d371434abfdd462c12a8f799aab33a3fa2bdf0f1
aziz zaaine <zaaine@live.fr>
2024-09-25 19:48:12 +0200

    ajout de react helmet pour le SEO

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import "../styles/_pages.scss/_home.scss/home.scss";
 import projectsData from "../assets/BDD_JSON/projects.json";
 
@@ -10,6 +11,18 @@ import Technologies from "../components/Technologies";
 function Home() {
   return (
     <div>
+      <Helmet>
+        <title>Accueil - Mon Portfolio</title>
+        <meta
+          name="description"
+          content="Bienvenue sur mon portfolio personnel."
+        />
+        <meta
+          name="keywords"
+          content="React, Portfolio, Développeur, Frontend, Php, html, css , Javascript, Junior, Freelance"
+        />
+        <link rel="canonical" href="https://www.porfolio.zaaine.com/" />
+      </Helmet>
       <Hero />
       <Carrousel projects={projectsData.projects} />
       <Technologies />
